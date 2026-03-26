46a10235749f9fa4eae58f881191e7388fc7853c
nivok-ai <yakovin.shura@mail.ru>
2026-03-26 17:48:06 +0300

    Убрал слабую ссылку при вызове сервисного класса

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,13 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
 
-    private CarServiceImpl carService;
+    private CarService carService;
 
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
